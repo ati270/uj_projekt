cc71764b52253d022a6503d624aee288c6021ffb
atii28 <dattila9006@gmail.com>
2021-02-20 00:16:21 +0100

    modified server.js

--- src/server.js
+++ src/server.js
@@ -3,12 +3,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist/investment-gui'));
+app.use(express.static(__dirname + '/dist/testapp'));
 
 //PATH LOCATION STARTEGY
 
 app.get('/*', function(req,res){
-  const fullPath = path.join(__dirname + '/dist/investment-gui/index.html');
+  const fullPath = path.join(__dirname + '/dist/testapp/index.html');
   console.log(" Fetching from.." + fullPath);
     res.sendFile(fullPath);
 })
